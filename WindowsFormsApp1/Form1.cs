ba3351080038f22fef676e0b393b2a240fda152a
lê duy khang <67537095+duykhang-bit@users.noreply.github.com>
2021-03-21 23:23:16 -0700

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (textBox1.Text == "admin" && textBox2.Text == "admin")
+            {
+                MessageBox.Show("thanh cong");
+                var frm = new Form2();
+                frm.Show();
+                this.Hide();
+            }                
+            else
+                MessageBox.Show("sai username hoac password");
+
+
+        }
+    }
+}
